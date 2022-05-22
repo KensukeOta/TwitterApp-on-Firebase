4502c245068bdfab96468f3913f4e8c82940d18d
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-22 14:37:04 +0900

    Googleアカウント認証を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { FC, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import styles from "./App.module.css";
+import Auth from "./components/Auth";
+import Feed from "./components/Feed";
+import { auth } from "./firebase";
+import { userInfo } from "./store/userInfo";
 
-function App() {
+const App: FC = () => {
+  const [user, setUser] = useRecoilState(userInfo);
+  
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser({
+          uid: authUser.uid,
+          photoUrl: authUser.photoURL,
+          displayName: authUser.displayName,
+        })
+      } else {
+        setUser({
+          uid: "",
+          photoUrl: "",
+          displayName: "",
+        })
+      }
+    })
+    return () => {
+      unSub();
+    }
+  }, []);
+  
   return (
-    <div className="App">
-    </div>
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
   );
 }
 
---
+++ src/components/Auth.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import styles from './Auth.module.css';
+import { auth, provider, storage } from '../firebase';
+import { signInWithPopup } from 'firebase/auth';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import SendIcon from '@mui/icons-material/Send';
+import CameraIcon from '@mui/icons-material/Camera';
+import EmailIcon from '@mui/icons-material/Email';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+const Auth: React.FC = () => {
+  const signInGoogle = async () => {
+    await signInWithPopup(auth, provider).catch((err: any) => alert(err.message));
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://images.unsplash.com/photo-1653056880006-2ed4fe8d20e0?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={signInGoogle}
+              >
+                SignIn with Google
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default Auth;
