538a6035e1fe9a17cf8b64bf9e65f13c00e0730e
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-24 21:42:07 +0900

    ツイート投稿機能のロジックを実装

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TweetInput.module.css";
-import { auth } from "../firebase";
+import { auth, storage, db } from "../firebase";
 import { useRecoilValue } from "recoil";
 import { userInfo } from "../store/userInfo";
-import { Avatar } from "@mui/material";
+import { Avatar, Button, IconButton } from "@mui/material";
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const TweetInput = () => {
   const user = useRecoilValue(userInfo);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (tweetImage) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + tweetImage.name;
+      const uploadTweetImg = uploadBytesResumable(
+        ref(storage, `images/${fileName}`),
+        tweetImage
+      );
+      uploadTweetImg.on(
+        "state_changed",
+
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await getDownloadURL(ref(storage, `images/${fileName}`)).then(
+            async (url) => {
+              addDoc(collection(db, "posts"), {
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timestamp: serverTimestamp(),
+                username: user.displayName,
+              });
+            }
+          );
+        }
+      );
+    } else {
+      addDoc(collection(db, "posts"), {
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timestamp: serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
 
   return (
     <div>
