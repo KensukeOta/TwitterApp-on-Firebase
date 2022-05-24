2ac5fe2c29462472d8014d216b6f6776b902f224
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-05-24 23:10:46 +0900

    Firestoreからツイートのデータを取得

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,48 @@
+import type { FC } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Feed.module.css";
-import { auth } from "../firebase";
+import { db } from "../firebase";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import TweetInput from "./TweetInput";
 
-const Feed = () => {
+const Feed: FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      avatar: "",
+      id: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+
+  useEffect(() => {
+    //Firebase ver9 compliant (modular)
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const unSub = onSnapshot(q, (snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          avatar: doc.data().avatar,
+          id: doc.id,
+          image: doc.data().image,
+          text: doc.data().text,
+          timestamp: doc.data().timestamp,
+          username: doc.data().username,
+        }))
+      );
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
+  
   return (
     <div className={styles.feed}>
-      Feed
       <TweetInput />
-      <button onClick={() => auth.signOut()}>Logout</button>
+      {posts.map((post) => (
+        <h3>{post.id}</h3>
+      ))}
     </div>
   );
 };
